a17d760c100f7d6985546f75b22ba3f35209dba6
firat <firat@reflecth.com>
2020-09-28 11:37:16 -0400

    initial commit

---
+++ Ulgena.Web.Sample/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using UlgenaApi;
+
+namespace Ulgena.Web.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("LinkCreate")]
+        [HttpGet]
+        public ActionResult LinkCreate()
+        {
+            return View("LinkCreate");
+        }
+
+        [Route("LinkCreate")]
+        [HttpPost]
+        public async Task<ActionResult> LinkCreatePost(string apiKey, string apiTitle, string apiStrategy,
+            string apiDestination)
+        {
+            var ulgena = new UlgenaApi.Ulgena(apiKey);
+
+            var strategy = UlgenaLink.ParseEnum<UlgenaLinkStrategy>(apiStrategy);
+
+            var linkResponse = await ulgena.LinkCreate(strategy, apiTitle, apiDestination);
+
+            ViewData["link_result"] = JsonConvert.SerializeObject(linkResponse, Formatting.Indented);
+
+            return View("LinkCreate");
+        }
+
+        [Route("LinkList")]
+        public ActionResult LinkList()
+        {
+            return View("LinkList");
+        }
+
+        [Route("LinkList")]
+        [HttpPost]
+        public async Task<ActionResult> LinkListPost(string apiKey)
+        {
+
+            var ulgena = new UlgenaApi.Ulgena(apiKey);
+
+            var linkResponse = await ulgena.LinkList();
+
+            ViewData["link_result"] = JsonConvert.SerializeObject(linkResponse, Formatting.Indented);
+
+            return View("LinkList");
+        }
+    }
+}
---
+++ Ulgena.Web.Sample/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+namespace Ulgena.Web.Sample
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
